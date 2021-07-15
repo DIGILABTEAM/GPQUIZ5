e105dc2a89f00cf5abb662d1c3bd86a7011d90af
Vikas Gavandi <vekgraphics@gmail.com>
2021-07-15 23:33:02 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,86 +1,47 @@
 /*All questions in the quiz goes here in the data object.....*/
 var data =
-[
 	[
-		{
-			"question"	: "What is a productive cough?",
-			"option1"	: "One that makes you feel better immediately",
-			"option2"	: "One that results in cancer",
-			"option3"	: "One that helps expel phlegm from the body",
-			"option4"	: "One that causes other symptoms to appear",
-			"answer"	: 3
-		},
-		{
-			"question"	: "In stack inserting an element is called _______",
-			"option1"	: "pop",
-			"option2"	: "push",
-			"option3"	: "create",
-			"option4"	: "evaluation",
-			"answer"	: 2
-		},
-		{
-			"question"	: "In Stack deleting an element is called __________",
-			"option1"	: "push",
-			"option2"	: "delete",
-			"option3"	: "remove",
-			"option4"	: "pop",
-			"answer"	: 4
-		},
-		{
-			"question"	: "In stack if user tries to remove an element from empty stack . It is ________",
-			"option1"	: "Empty collection",
-			"option2"	: "Garbage collection",
-			"option3"	: "Overflow of stack",
-			"option4"	: "Underflow of stack",
-			"answer"	: 4
-		},
-		{
-			"question"	: "If user pushes an element in stack which is full then stack becomes ________",
-			"option1"	: "underflow",
-			"option2"	: "overflow",
-			"option3"	: "userflow",
-			"option4"	: "crash",
-			"answer"	: 2
-		},
-		{
-			"question"	: "In order to keep track of current top most element we need to maintain a variable",
-			"option1"	: "True",
-			"option2"	: "False",
-			"option3"	: "Not Necessarily",
-			"option4"	: "User choice",
-			"answer"	: 1
-		},
-		{
-			"question"	: "The variable denoting topmost element of stack is ____",
-			"option1"	: "first",
-			"option2"	: "Top",
-			"option3"	: "stack",
-			"option4"	: "data",
-			"answer"	: 2
-		},
-		{
-			"question"	: "The Top is initialized as _________",
-			"option1"	: "-1",
-			"option2"	: "1",
-			"option3"	: "3",
-			"option4"	: "0",
-			"answer"	: 1
-		},
-		{
-			"question"	: "Stack is a _________",
-			"option1"	: "Linear data structure",
-			"option2"	: "Non-Linear Data structure",
-			"option3"	: "Multiple Data structure",
-			"option4"	: "None",
-			"answer"	: 1
-		},
-		{
-			"question"	: "Stack is also called as _______",
-			"option1"	: "Non-Restricted DS",
-			"option2"	: "Restricted DS",
-			"option3"	: "Anti-Restricted",
-			"option4"	: "None",
-			"answer"	: 2
-		},
-			]
-];
+		[
+			{
+				"question": "11.	A cough made worse in recumbent position suggests:",
+				"option1": "underflow",
+				"option2": "overflow",
+				"option3": "userflow",
+				"option4": "crash",
+				"answer": 2
+			},
+			{
+				"question": "Most common cause of a chronic slightly productive cough in the adult population is:",
+				"option1": "True",
+				"option2": "False",
+				"option3": "Not Necessarily",
+				"option4": "User choice",
+				"answer": 1
+			},
+			{
+				"question": "13.	Which of the following is an expectorant?",
+				"option1": "first",
+				"option2": "Top",
+				"option3": "stack",
+				"option4": "data",
+				"answer": 2
+			},
+			{
+				"question": "14.	A 55-year-old man with a tickly cough with accompanying sore throat for two days. He hasn't tried anything and it's keeping him awake. He is not on any other medication except eye drops for his glaucoma. What would you recommend?",
+				"option1": "-1",
+				"option2": "1",
+				"option3": "3",
+				"option4": "0",
+				"answer": 1
+			},
+			{
+				"question": "15.	Which of the following is a cough suppressant?",
+				"option1": "Linear data structure",
+				"option2": "Non-Linear Data structure",
+				"option3": "Multiple Data structure",
+				"option4": "None",
+				"answer": 1
+			},
+
+		]
+	];
